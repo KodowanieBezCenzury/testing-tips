a8f35d838f685f40dcd83f9b3a286a091b164208
smalaca <ab13.krakow@gmail.com>
2021-07-29 22:23:25 +0200

    Decrease visiblity of domain object constructors

--- shared-given/src/main/java/com/kbc/apartmentsapp/owner/OwnerId.java
+++ shared-given/src/main/java/com/kbc/apartmentsapp/owner/OwnerId.java
@@ -1,11 +1,12 @@
 package com.kbc.apartmentsapp.owner;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.UUID;
 
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Getter
 public class OwnerId {
     private final UUID id;
